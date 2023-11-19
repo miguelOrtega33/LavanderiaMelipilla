669b643c2886ef1ebd2b1ad8ed84fc16db59d430
Miguel Ortega <miguelortega805@gmail.com>
2023-11-18 23:25:53 -0300

    local storage informe y registro

--- src/app/informes/informes.page.ts
+++ src/app/informes/informes.page.ts
@@ -9,6 +9,20 @@ export class InformesPage implements OnInit {
 
   constructor() { }
 
+  // Local storage del informe
+  tipo = localStorage.getItem('tipo');
+  servicio = localStorage.getItem('servicio');
+  registro = localStorage.getItem('registro');
+  codigo = localStorage.getItem('codigo');
+  cantidad = localStorage.getItem('cantidad');
+  usuario = localStorage.getItem('nombreUsuario');
+
+  // Datos para la fecha
+  fecha: Date = new Date();
+  dia: number = this.fecha.getDate();
+  mes: number = this.fecha.getMonth() + 1;
+  annio: number = this.fecha.getFullYear();
+
   ngOnInit() {
   }
 
--- src/app/registrar/registrar.page.ts
+++ src/app/registrar/registrar.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registrar',
@@ -7,9 +10,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarPage implements OnInit {
 
-  constructor() { }
+  formularioRopa: FormGroup;
+
+  constructor(private router: Router, public fb: FormBuilder, private alertController: AlertController,  private navCtrl: NavController) {
+    this.formularioRopa = this.fb.group({
+      'tipo': new FormControl("", Validators.required),
+      'servicio': new FormControl("", Validators.required),
+      'registro': new FormControl("", Validators.required),
+      'codigo': new FormControl("", Validators.required),
+      'cantidad': new FormControl("", Validators.required)
+    })
+  }
 
   ngOnInit() {
+  }
+  async registrar() {
+    var f = this.formularioRopa.value;
+
+    if (this.formularioRopa.invalid) {
+      const alert = await this.alertController.create({
+        header: 'Atencion!.',
+        message: 'Debes ingresar todos los datos',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+      return;
+    } else {
+
+      var tipo = f.tipo;
+      var servicio = f.servicio;
+      var registro = f.registro;
+      var codigo = f.codigo;
+      var cantidad = f.cantidad;
+
+      localStorage.setItem('tipo', tipo);
+      localStorage.setItem('servicio', servicio);
+      localStorage.setItem('registro', registro);
+      localStorage.setItem('codigo', codigo);
+      localStorage.setItem('cantidad', cantidad);
+
+
+
+      const alert = await this.alertController.create({
+        header: 'Datos correctos',
+        message: 'Lote de ropa ingresada',
+        buttons: [{
+          text: 'OK',
+          handler: () => {
+            this.navCtrl.navigateForward('folder/:id');
+          }
+        }]
+      });
+
+      await alert.present();
+    }
   }
 
 }
